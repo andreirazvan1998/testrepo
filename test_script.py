3ebe16f1fa657c07e633fd595ffa4c400136afc2
BOLBOCEANU ANDREI-RAZVAN <andrei-razvan.bolboceanu@continental-corporation.com>
2025-03-12 12:42:59 +0200

    test

--- test_script.py
+++ test_script.py
@@ -16,7 +16,7 @@ def test_add_numbers():
     assert add_numbers(0, 0) == 0
 
 def test_multiply_numbers():
-    assert multiply_numbers(2, 3) == 5
+    assert multiply_numbers(2, 3) == 6
     assert multiply_numbers(-1, 5) == -5
     assert multiply_numbers(0, 10) == 0
     assert multiply_numbers(4, 4) == 16
